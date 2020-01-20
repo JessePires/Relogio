1bb88e1f11d39f60bb06187b93b761c416d9fad0
Jesse Pires <jessepires2010@gmail.com>
2020-01-20 17:06:13 -0300

    Relogio

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Relogio
-Relógio desenvolvido em Javascript para estudos
+
+Feito em Javascript por motivo de estudos.
+
+# Funcionamento
+
+O horário exibido é obtido do computador.
+De acordo com o a hora atual, uma imagem é exibida e uma cor de background é setada
+para representar um período do dia (manhã, tarde ou noite).
---
+++ script.js
@@ -0,0 +1,33 @@
+
+function relogio(){
+
+    var date = new Date();
+    var horario = window.document.querySelector("div#horario");
+    var pRelogio = window.document.querySelector("p#relogio");
+    pRelogio.innerHTML = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    setTimeout("relogio()", 1000);
+
+    var h = date.getHours();    
+
+    if(h >= 0 && h < 12){
+
+        horario.style.backgroundImage = "url('imagens/fotomanha.png')";
+        pRelogio.style.color = "#181818";
+
+    }else if(h >= 12 && h < 18){
+
+        horario.style.backgroundImage = "url('imagens/fototarde.png')";
+        document.body.style.background = "#b46f36";
+        pRelogio.style.color = "#bebaba";        
+
+    }else{
+
+        horario.style.backgroundImage = "url('imagens/fotonoite.png')";
+        document.body.style.background = "#acaaaa";
+        pRelogio.style.color = "#a1a1a1";       
+
+    }
+
+}
+
+relogio(); 
